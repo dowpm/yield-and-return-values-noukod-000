5d626c30e55ce3b34d1770067092c2041e823618
Mac Dowell Prosper <macdowell@noukod.com>
2018-09-20 15:50:37 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,10 +4,11 @@ def hello(array)
   i = 0
   collection = []
   while i < array.length
-    binding.pry
+    # binding.pry
     collection << yield(array[i])
     i += 1
   end
+  collection
 end
 
 hello(["Tim", "Tom", "Jim"]) { |name|  "Hi, #{name}" }
